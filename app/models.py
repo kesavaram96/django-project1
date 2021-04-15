da8a7ff0687c979c2fb1bec159c35b144175b28c
RK_Raam <k7raam.stu@gmail.com>
2021-04-15 09:42:29 +0530

    First app created by DJango
    the project is for my practice

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    UId=models.IntegerField()
+    UName=models.CharField(max_length=30)
+    EMail=models.EmailField()
+    password=models.CharField(max_length=10)
+    
